8c595f86fc2e6fec996080061324a074a0a56bd6
Rudra Sarkar <rudrasarkar815@gmail.com>
2020-11-19 01:24:20 +0600

    static and dynamic variable understanding..

--- Practice2.go
+++ Practice2.go
@@ -6,11 +6,22 @@ func main() {
 	// Data Types
 
 	//var variable_list optional_data_type;
-	var Age = 5.5;
+	var Age = 5.5; // Static
+
+	Name := "Rudra" // Dynamic := meaning is var in shorthand
+	a, b, c := 1, true, "A new day"
 
 	fmt.Println(Age)
+	fmt.Printf("Age type is: %T\n", Age)
 
-	fmt.Printf("Age type is: %T", Age)
+
+	fmt.Println(Name)
+	fmt.Printf("Name type is: %T\n", Name)
+
+	fmt.Printf("a type is %T\n", a)
+	fmt.Printf("b type is %T\n", b)
+	fmt.Printf("c type is %T\n", c)
+
 }
 
 // Note: I have confussion about "Derived types".
