e1ca3c4498bffd52d7344d711708fd1195c36bd9
Rudra Sarkar <rudrasarkar815@gmail.com>
2020-11-19 00:57:09 +0600

    Practice 1

--- Practice1.go
+++ Practice1.go
@@ -1,3 +1,19 @@
+// Package Declaration
+package main
+
+// Import packages
+import "fmt" // https://golang.org/pkg/fmt/
+
+// program execution begins
 func main() {
-	
+	// this is comment
+	fmt.Println("Hello, There This is Practice number 1")
 }
+
+// Function
+
+// Variable
+
+// Statements and Expressions
+
+// Comment
