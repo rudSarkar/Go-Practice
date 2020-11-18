53498550e8b866a9898c92ba3a9f9134fcf0bf0e
Rudra Sarkar <rudrasarkar815@gmail.com>
2020-11-19 01:17:18 +0600

    Update Practice 2
    I have confussion about derived types

---
+++ Practice2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Data Types
+
+	//var variable_list optional_data_type;
+	var Age = 5.5;
+
+	fmt.Println(Age)
+}
+
+// Note: I have confussion about "Derived types".
+/*
+They include:
+	(a) Pointer types
+	(b) Array types
+	(c) Structure types
+	(d) Union types
+	(e) Function types
+	f) Slice types
+	g) Interface types
+	h) Map types
+	i) Channel Types
+*/
